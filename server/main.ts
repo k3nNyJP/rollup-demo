73710fb3dd5be2a51c40034cccdb9f2a596f0398
k3nNy 51rcy <k3nny.51rcy@gmail.com>
2019-02-04 10:13:57 +0900

    [feat] add GET version API

--- server/main.ts
+++ server/main.ts
@@ -1,10 +1,17 @@
-import * as express from "express";
-import { join } from "path";
+import * as express from 'express';
+import { join } from 'path';
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use("/", express.static(join(__dirname, "../rollup-demo")));
+app.use('/', express.static(join(__dirname, '../rollup-demo')));
+
+app.get('/api/version', (req, res) => {
+  res.json({
+    version: '1.0.0',
+    timestamp: Date.now(),
+  }).end();
+});
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
