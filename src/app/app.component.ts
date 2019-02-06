f4beca472c2c9651ec26bd90b380b75562c609e7
Kenya Kawasaki <kawasaki.kenya@mail.glory.co.jp>
2019-02-06 10:37:43 +0900

    [feat] swap image if api version gte 2.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { VersionService, Version } from "./version.service";
-import { Observable } from 'rxjs';
+import { VersionService, Version } from './version.service';
+import { Observable, of } from 'rxjs';
+import { lt } from 'semver';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,16 +12,15 @@ import { Observable } from 'rxjs';
 export class AppComponent {
   public title = 'rollup-demo';
   public version: Observable<Version>;
+  public isAngular: Observable<boolean> = of(false);
   public host: string;
   public port: number;
 
   constructor(public versionService: VersionService) { }
 
-  public ngOnInit() {
-  }
-
   public configureServer() {
     this.versionService.setServer(this.host, this.port);
     this.version = this.versionService.subscribeVersion();
+    this.isAngular = this.versionService.isAngular();
   }
 }
--- src/app/version.service.ts
+++ src/app/version.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 import { Observable, interval } from 'rxjs';
-import { switchMap, share, map, startWith, filter } from "rxjs/operators";
-import * as moment from "moment";
+import { switchMap, share, map, startWith } from 'rxjs/operators';
+import * as moment from 'moment';
+import { gte } from 'semver';
 
 export interface Version {
   version: string;
@@ -18,7 +19,7 @@ export class VersionService {
   constructor(private http: HttpClient) { }
 
   subscribeVersion(): Observable<Version> {
-    return interval(5000).pipe(
+    return interval(1000).pipe(
       startWith(0),
       switchMap(() => this.http.get<any>(this.server)),
       map((data) => ({ version: data.version, timestamp: moment(data.timestamp).toLocaleString() })),
@@ -28,5 +29,11 @@ export class VersionService {
 
   setServer(host: string, port: number) {
     this.server = `http://${host}:${port}`;
+  }
+
+  isAngular(): Observable<boolean> {
+    return this.subscribeVersion().pipe(
+      map((ver) => gte(ver.version, '2.0.0')),
+    );
   }
 }
